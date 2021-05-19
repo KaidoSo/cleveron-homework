82546762c7834be0d9299e15d63159e081655ce6
KaidoSo <kaido.soomlais@gmail.com>
2021-05-19 18:29:00 +0300

    added logo and material template form

--- problem_1c/signup-form/src/FormSignup.js
+++ problem_1c/signup-form/src/FormSignup.js
@@ -1,11 +1,134 @@
-import React from 'react'
+import React from 'react';
+import Cleveron from './cleveron-logo.png';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import './App.css';
 
-const FormSignup = () => {
+function Copyright() {
+
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
-    )
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://cleveron.eu/et" target="_blank" rel="noreferrer">
+                Cleveron Homework
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default FormSignup
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+        submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignUp() {
+const classes = useStyles();
+
+return (
+    <Container component="main" maxWidth="xs">
+    <CssBaseline />
+        <div className={classes.paper}>
+            <a className="logo-link" href="https://cleveron.eu/et" target="_blank" rel="noreferrer">
+                    <img className='cleveron-logo' src={Cleveron} alt="cleveron logo" />
+            </a>
+            <Typography component="h1" variant="h5">
+                Sign up
+            </Typography>
+            <form className={classes.form} noValidate>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            autoComplete="name"
+                            name="name"
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Name"
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="confirm-password"
+                            label="Confirm Password"
+                            type="password"
+                            id="confirmPassword"
+                            autoComplete="confirm-password"
+                        />
+                    </Grid>
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                >
+                    Sign Up
+                </Button>
+                <Grid container justify="flex-end">
+                    <Grid item>
+                        <Link href="#" variant="body2">
+                            Already have an account? Sign in
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </div>
+        <Box mt={5}>
+        <Copyright />
+        </Box>
+    </Container>
+    );
+}
